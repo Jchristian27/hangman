34fba273cfa56433e15300f6d9b196e57f30286e
Jonathan Christian <christianj707@gmail.com>
2022-11-13 09:46:06 -0800

    update the file name that gets loaded

--- random_word.rb
+++ random_word.rb
@@ -5,7 +5,7 @@ class RandomWord
   attr_reader :random_word
 
   def initialize
-    @word_array = File.open('google-10000-english-no-swears.txt').to_a
+    @word_array = File.open('words.txt').to_a
     @word_array.filter! { |e| e != nil && (5..12).include?(e.strip.size) }
     @random_word = @word_array[rand(1..@word_array.size)]
   end
