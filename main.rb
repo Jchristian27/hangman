6b03b929738e117bda9540bb517b8b8259b3330e
Jonathan Christian <christianj707@gmail.com>
2022-11-13 11:01:24 -0800

    Create intial terminal output that preceeds a game

--- main.rb
+++ main.rb
@@ -3,5 +3,22 @@
 require_relative 'game'
 require_relative 'random_word'
 
-puts 'Welcome to hangman!'
-Game.new
+puts 'How to play Hangman in the console.'
+puts 'A random word with 5-12 letters will be chosen. On each turn, you can guess one letter.'
+puts 'To win, you must find all the letters in the word before using 8 incorrect guesses.'
+puts "Let's play hangman in the console! Would you like to:"
+
+puts '[1] Play a new game'
+puts '[2] Load a saved game'
+
+answer = gets.chomp.to_i
+until answer.between?(1, 2)
+  puts 'Error. Please enter a valid answer.'
+  answer = gets.chomp.to_i
+end
+case answer
+when 1
+  Game.new
+when 2
+  puts 'Loading file!'
+end
