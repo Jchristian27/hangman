20ea1746b89f5a431c70368ffece468a4b35b4ad
Jonathan Christian <christianj707@gmail.com>
2022-11-12 20:23:45 -0800

    Build basic Game and RandomWord classes

---
+++ main.rb
@@ -0,0 +1,16 @@
+class RandomWord
+  attr_reader :random_word
+  def initialize
+    @word_array = File.open('google-10000-english-no-swears.txt').to_a
+    @word_array.filter! { |e| e != nil && (5..12).include?(e.strip.size) }
+    @random_word = @word_array[rand(1..@word_array.size)]
+  end
+end
+
+class Game
+  def initialize
+    puts RandomWord.new.random_word
+  end
+end
+
+Game.new
