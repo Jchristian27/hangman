29a03d6a2ac4aeaf082aea8f50e146eb62852984
Jonathan Christian <christianj707@gmail.com>
2022-11-14 18:19:41 -0800

    Move turn logic into own methods and refactor

--- game.rb
+++ game.rb
@@ -5,11 +5,12 @@ class Game
   def initialize
     @word = RandomWord.new.random_word
     @length = @word.length
-    @guess_array = []
     @word_array = @word.split('')
     @word_array.pop
+    @guess_array = []
     @guess = nil
-    @round = 0
+    @wrong_letters = []
+    @wrong_guesses = 0
     puts "\nYour word has been chosen. It has #{@length - 1} characters\n\n"
     (@length - 1).times do
       @guess_array.push('_')
@@ -22,32 +23,46 @@ class Game
   end
 
   def warning
-    case @round
-    when 10
+    case @wrong_guesses
+    when 6
       puts 'Two guesses remaining.'
-    when 11
-      puts 'BE CAREFUL. You only have one guess left.' if @round == 11
+    when 7
+      puts 'BE CAREFUL. You only have one guess left.'
     end
+  end
+
+  def this_round_logic(letter)
+    if @word_array.include?(letter)
+      @word_array.each_with_index do |e, i|
+        @guess_array[i] = letter if e == letter
+      end
+    elsif !@word_array.include?(letter)
+      @wrong_letters.push(letter)
+      @wrong_guesses += 1
+    end
+    this_round_results
+  end
+
+  def this_round_results
+    puts "\n#{@guess_array.join(' ')}\n\n"
+    puts "Incorrect letters guessed: #{@wrong_letters.join(' ')}\n\n"
   end
 
   def player_turn
-    #p @word_array
     warning
-    puts 'Guess a letter'
+    puts 'Your turn to guess a letter in the secret word.'
+    puts "You can also type 'save' or 'exit' to leave the game.\n\n"
     letter = gets.chomp
-    until letter.length == 1
-      puts 'Uh oh. You entered more than one letter. Please try again'
+    until letter.length == 1 && letter.match?(/[[:alpha:]]/) &&
+          !@wrong_letters.include?(letter) && !@guess_array.include?(letter)
+      puts 'Uh oh. You either entered an invalid letter. Please enter a letter you haven\'t picked yet'
       letter = gets.chomp
     end
-    @round += 1 unless @word_array.include?(letter)
-    @word_array.each_with_index do |e, i|
-      @guess_array[i] = letter if e == letter
-    end
-    puts @guess_array.join(' ')
+    this_round_logic(letter)
   end
 
   def game_over?
-    true if @round == 12 || @guess_array == @word_array
+    true if @wrong_guesses == 8 || @guess_array == @word_array
   end
 
   def play_game
